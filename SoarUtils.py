c65ced85dc0d3d1bcd2a74bf72beb1070152bd49
Aaron Mininger <mininger@umich.edu>
2019-04-04 14:18:54 -0400

    Added utility method to remove a tree from working memory

--- SoarUtils.py
+++ SoarUtils.py
@@ -37,3 +37,19 @@ class SoarUtils:
             wme = wme_table[child_name]
             wme.set_value(value)
             wme.update_wm(root_id)
+
+    def remove_tree_from_wm(wme_table):
+        """
+        Given a wme_table filled by SoarUtils.update_wm_from_tree, removes all wmes from working memory 
+
+        Intermediate nodes are sml.Identifiers, which are removed from the table
+        Leaves are SoarWME's which are kept in the table but .remove_from_wm() is called on them
+        """
+        items_to_remove = set()
+        for path, wme in wme_table.items():
+            if isinstance(wme, sml.Identifier):
+                items_to_remove.add(path)
+            else:
+                wme.remove_from_wm()
+        for path in items_to_remove:
+            wme_table.remove(path)
